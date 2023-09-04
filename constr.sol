92eddb5e40799c418b6e0a25a7f0a9c80527b5ac
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2023-09-05 00:50:33 +0530

    Update constr.sol

--- constr.sol
+++ constr.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
 
+pragma solidity >=0.8.2 <0.9.0;
+
+contract alex{
+  uint public x=10;
+  constructor (){
+      x=20;
+  }
+}
+
+
+
+
+// contract alex{
+//   uint public x=10;
+//   constructor (uint _x){
+//       x=_x;
+//   }
+// }
