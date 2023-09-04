ebbf9ef1ed014b47d9bc5e6022c6bc24e7af762e
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2023-09-05 01:06:22 +0530

    Update constr1.sol

--- constr1.sol
+++ constr1.sol
@@ -1,1 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Const {
+    uint public x;
+    uint public y;
+
+    constructor(uint _x, uint _y) {
+        x = _x;
+        y = _y;
+    }
+}
 
