caf575726c2bd1632417e618707c77ee24577f07
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2023-10-07 00:22:42 +0530

    Update Inheritance.sol

--- OOPs/Inheritance.sol
+++ OOPs/Inheritance.sol
@@ -1,1 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0
 
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Model{ 
+    //Parent
+    string public name="NYC";
+    uint public length; 
+    uint public height; 
+    uint public breadth;
+
+function welcomMessage() public pure returns(string memory){
+    return "Hello to NYC";
+}
+}
+
+contract NewModal is Model{ 
+    //Child
+
+    bool public heatResitant= true; 
+
+    function greetings() public pure returns(string memory){ 
+        return "Good Morning"; 
+        }
+
+}
