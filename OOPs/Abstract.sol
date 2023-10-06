219b4e57708ac54cf97b2fde4ce57ce7ec04475a
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2023-10-07 00:25:34 +0530

    Update Abstract.sol

--- OOPs/Abstract.sol
+++ OOPs/Abstract.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
 
+pragma solidity >=0.7.0 <0.9.0;
+
+abstract contract Model{
+
+function sub(uint a,uint b) public pure returns(uint){ 
+    return a-b; 
+    } 
+    function add(uint a,uint b) public pure virtual returns(uint); 
+    }
+
+contract developer is Model{ 
+    function add(uint a,uint b) public pure override returns(uint){ 
+    return a+b; 
+    } 
+}
