3c150c90c53fca393562fe58cbeabc3fadb33ea8
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2023-10-07 00:09:10 +0530

    Add files via upload

---
+++ OOPs/obj_class.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0<0.9.0;
+
+contract Book{
+    uint length;
+    uint breadth;
+    uint height;
+
+
+    function setDimension(uint _length,uint _breadth,uint _height) public {
+          length=_length;
+          breadth=_breadth;
+          height=_height;
+    }
+
+    function getDimension() public view returns(uint,uint,uint){
+        return (length,breadth,height);
+    }
+}
+
+
+contract Demo{
+    Book obj=new Book();
+
+    function getObject() public view returns (Book){
+        return obj;
+    }
+
+    function writeDimension(uint _length, uint _breadth, uint _height) public {
+        obj.setDimension(_length, _breadth, _height);
+    }
+
+    function readDimension() public view returns (uint,uint,uint){
+        return obj.getDimension();
+    }
+}
