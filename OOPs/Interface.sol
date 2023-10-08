f71a59c294c02d205b11188b5025572e8f4dfc78
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2023-10-08 23:10:26 +0530

    Update Interface.sol

--- OOPs/Interface.sol
+++ OOPs/Interface.sol
@@ -1,1 +1,14 @@
 
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+interface car{
+     function gear() external pure returns(uint); 
+     }
+
+contract superCar is car{ 
+    function gear() external pure returns(uint){
+         return 4;
+        } 
+    }
