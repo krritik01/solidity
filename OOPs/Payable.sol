19853804503893d7c75eace6ad497d7cc1deb618
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2023-10-08 23:16:26 +0530

    Update Payable.sol

--- OOPs/Payable.sol
+++ OOPs/Payable.sol
@@ -1,1 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
 
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Demo{ //transer ether to my contract 
+          function sendEtherToContract() public payable {
+
+                }
+
+          function balanceOfContract() public view returns(uint){
+             return address(this).balance; 
+             }
+        }
