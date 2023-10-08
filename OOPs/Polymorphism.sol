35d2fa42af63473918770474d194a6dca30a81d3
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2023-10-09 00:20:53 +0530

    Update Polymorphism.sol

--- OOPs/Polymorphism.sol
+++ OOPs/Polymorphism.sol
@@ -1,13 +1,17 @@
-// SPDX-License-Identifier: GPL-3.0
-
-pragma solidity >=0.7.0 <0.9.0;
+// SPDX-License-Identifier: MIT
+pragma solidity <0.9.0;
 
 contract demo{
-    function sun(uint a, uint b) public pure returns (uint){
+    function sum(uint a, uint b) public pure returns(uint){
         return a+b;
     }
 
-    function sum(uint a, uint b, uint c) public pure returns (uint){
+    function sum(uint a, uint b, uint c) public pure returns(uint){
         return a+b+c;
+    }
+
+    function sum(string memory a, string memory b) public pure returns(string memory){
+        b="It's Ritik here";
+        return a;
     }
 }
