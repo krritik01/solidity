c286863ad4fb3eaa1d3c886f66030db5742e6567
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2023-10-10 00:57:31 +0530

    Update Fallback.sol

--- OOPs/Fallback.sol
+++ OOPs/Fallback.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity <0.9.0;
 
+contract Fallback{
+
+    function payEther() payable public {
+
+    }
+
+    function check_balance() public view returns(uint){
+        return address(this).balance;
+    }
+    
+    receive() external payable {
+        // handle incoming ether and to don't get error
+     }
+
+    fallback() external payable {
+        payable (msg.sender).transfer(msg.value);
+    }
+}
