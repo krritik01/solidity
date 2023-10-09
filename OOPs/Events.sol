87c57941c22fc3dc67d3c870650b3c596df8e77e
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2023-10-10 00:15:49 +0530

    Update Events.sol

--- OOPs/Events.sol
+++ OOPs/Events.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0<0.9.0;
-contract demo{
- string public str;
- event register(address manager,string char);
- function setter(string memory _str) public {
-    str=_str;
+pragma solidity <0.9.0;
+
+contract Event{
+       string public str;
+    event register(address manager, string char);
+
+    function setter(string memory _str) public {
+        str=_str;
     emit register(msg.sender, str);
- }
- }
+    }
+}
