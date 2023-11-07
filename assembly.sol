8255b48a459264aa565b2f6b3db2191744c3e2ac
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2023-11-07 15:57:47 +0530

    Update assembly.sol

--- assembly.sol
+++ assembly.sol
@@ -1,25 +1,48 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.16 <0.9.0;
 
-library Sum {   
-   function sumUsingInlineAssembly(uint[] memory _data) public pure returns (uint o_sum) {
-      for (uint i = 0; i < _data.length; ++i) {
-         assembly {
-            o_sum := add(o_sum, mload(add(add(_data, 0x20), mul(i, 0x20))))
-         }
-      }
-   }
-}
-contract Test {
-   uint[] data;
-   
-   constructor() public {
-      data.push(1);
-      data.push(2);
-      data.push(3);
-      data.push(4);
-      data.push(5);
-   }
-   function sum() external view returns(uint){      
-      return Sum.sumUsingInlineAssembly(data);
-   }
+
+library VectorSum {
+    // This function is less efficient because the optimizer currently fails to
+    // remove the bounds checks in array access.
+    function sumSolidity(uint[] memory data) public pure returns (uint sum) {
+        for (uint i = 0; i < data.length; ++i)
+            sum += data[i];
+    }
+
+    // We know that we only access the array in bounds, so we can avoid the check.
+    // 0x20 needs to be added to an array because the first slot contains the
+    // array length.
+    function sumAsm(uint[] memory data) public pure returns (uint sum) {
+        for (uint i = 0; i < data.length; ++i) {
+            assembly {
+                sum := add(sum, mload(add(add(data, 0x20), mul(i, 0x20))))
+            }
+        }
+    }
+
+    // Same as above, but accomplish the entire code within inline assembly.
+    function sumPureAsm(uint[] memory data) public pure returns (uint sum) {
+        assembly {
+            // Load the length (first 32 bytes)
+            let len := mload(data)
+
+            // Skip over the length field.
+            //
+            // Keep temporary variable so it can be incremented in place.
+            //
+            // NOTE: incrementing data would result in an unusable
+            //       data variable after this assembly block
+            let dataElementLocation := add(data, 0x20)
+
+            // Iterate until the bound is not met.
+            for
+                { let end := add(dataElementLocation, mul(len, 0x20)) }
+                lt(dataElementLocation, end)
+                { dataElementLocation := add(dataElementLocation, 0x20) }
+            {
+                sum := add(sum, mload(dataElementLocation))
+            }
+        }
+    }
 }
