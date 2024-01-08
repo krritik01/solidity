3b17ed8b7738372892882a0c1b433752443bef32
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2024-01-09 02:18:59 +0530

    Create IR.sol

---
+++ IR.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.1;
+
+contract A {
+    uint x;
+    constructor() {
+        x = 42;
+    }
+    function f() public view returns(uint256) {
+        return x;
+    }
+}
+contract B is A {
+    uint public y = f();
+}
